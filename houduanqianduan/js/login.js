119cf5c92caac78a567603576fd2ea2447f57cf3
tenghuan123 <2540149177@qq.com>
2018-11-07 16:59:19 +0800

    更新

--- houduanqianduan/js/login.js
+++ houduanqianduan/js/login.js
@@ -1,5 +1,38 @@
-$(function () {
-    var $window = $(window);
-    var $app = $("#app");
-    $app.css({"width":$window.width(),"height":$window.height()});
+$(function(){
+	var $form = $("#form");
+	var $btnlg = $(".btn-lg");
+	
+init();
+function init(){
+	$btnlg.click(function(){
+		var username = $form.children("input").eq(0).val();
+		var password = $form.children("input").eq(1).val();
+		
+		var data={
+			'username':username,
+			'password':password
+		};
+		initajax('post','examination/backgroundAPI/login',data,function(data){
+			console.log(data);
+			alert("登陆成功");
+			window.location.href="index.html";
+		},function(err){
+			console.log(err);
+			alert("登陆失败，请检查密码是否正确");
+		})
+	})
+}
+function initajax(methods,url,data,fun,Err){
+	$.ajax({
+		type:methods,
+		url:url,
+		data:data,
+		success:function(data){
+			fun(data);
+		},
+		error:function(err){
+			Err(err);
+		}
+	})
+}
 })
