713a0139ab90b9ec29926864ccd67afb58a97932
Danicady <504830697@qq.com>
2018-11-13 09:26:34 +0800

    搜索单一信息

--- backgroundAPI/search.php
+++ backgroundAPI/search.php
@@ -2,20 +2,23 @@
 /**
  * Created by PhpStorm.
  * User: DY
- * Date: 2018/11/8
- * Time: 16:17
+ * Date: 2018/11/12
+ * Time: 17:21
  */
 require 'fun_con_db.php';
 if ($_GET) {
-    $userID = $_GET['userID'];
+    $name = $_GET['name'];
     $con = db_con();
     $sql="select  department, e.userID, name,superior, classname,number,tname, cname,nature from
 (select  r.userID,class.classID,class.classname,class.number,class.tname,c.courseID,c.cname,c.nature from relationship r join course c on r.courseID=c.courseID
-join class on class.classID=r.classID) e join (select department,userID,name,superior from user where userID='{$userID}') u on e.userID=u.userID";
+join class on class.classID=r.classID) e join (select department,userID,name,superior from user where name='{$name}') u on e.userID=u.userID";
     if($res=mysqli_query($con,$sql)){
         $data=[];
         while ($row=mysqli_fetch_assoc($res))
             $data[]=$row;
         echo json_encode($data);
+    }else{
+        header("HTTP/1.1 401");
+        echo json_encode(array("message"=>"unauthorized token"));
     }
 }
